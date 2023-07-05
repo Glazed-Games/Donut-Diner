fb7746aa75abf3d64dd24801e32b77d9bdc368b0
Kara Thrash <dvdonato@gmail.com>
2023-07-05 14:55:26 -0400

    adding yarnspinner for dialogue

--- Assets/Code/Scripts/Framework/DialogueManager.cs
+++ Assets/Code/Scripts/Framework/DialogueManager.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using UnityEngine;
+
 namespace DonutDiner.FrameworkModule
 {
     public class DialogueManager : ComponentSingleton<DialogueManager>
@@ -8,6 +11,8 @@ namespace DonutDiner.FrameworkModule
 
         private string _currentScene = "null";
         private bool _reloadSaveFile = false;
+        private DialogueObject activeDialogue;
+        private int currentLine;
 
         #endregion Fields
 
@@ -39,6 +44,64 @@ namespace DonutDiner.FrameworkModule
         {
         }
 
+
+
+
+
         #endregion Protected Methods
+
+        #region Public Methods
+
+        public void StartDialogue(DialogueObject dialogue)
+        {
+            //display text
+            //tell audio to play voice line
+            //start timer to show responses or display next line of the speech
+            currentLine = 0;
+
+        }
+
+        public void AdvanceDialogue( )
+        {
+            if (activeDialogue == null) { return; }
+
+            currentLine++;
+
+            if (activeDialogue.Dialogue.Length > currentLine)
+            {
+                //show next line
+                return;
+            }
+
+            if (activeDialogue.HasResponses)
+            {
+                //show response
+                return;
+            }
+
+            //otherwise end dialogue
+        }
+
+
+        public void DialogueResponse(int responseSelected)
+        {
+            if (activeDialogue == null) { return; }
+            if (activeDialogue.HasResponses == false) { return; }
+        }
+        
+
+
+
+        #endregion Public Methods
     }
+
+
+
+
+
+
+
+
+
+
 }
